466d364e4e9d0d41e672284b375e6edcb3f8028a
Brig101 <blcardon99@gmail.com>
2022-03-23 21:08:02 -0600

    replaced r code

--- updateLabels.py
+++ updateLabels.py
@@ -26,10 +26,13 @@ os.environ['PYTHONPATH'] = r'C:\Users\GISUser\anaconda3\Lib\site-packages'
 os.environ['R_HOME'] = r'C:\Users\GISUser\anaconda3\Lib\R'
 os.environ['R_USER'] = r'C:\Users\GISUser\anaconda3\envs\rstudio'
 # This makes R work
-import rpy2.robjects as ro
+#import rpy2.robjects as ro
 import time
 import glob
 from datetime import datetime
+from labels import labels
+from labels import leaseLabel
+
 
 # Function that finds the element and waits for it to load before continuing
 
@@ -101,23 +104,33 @@ def updateLabels(webscrape, workingInterestFile, ownershipFile):
         selowner.click()
         bar = findElement(f'//*[@id="data-picker-{ids}-simple"]/div/input[2]')
         bar.click()
-        fourx = findElement(r'//*[@id="4108947"]')
-        fourx.click()
+        #fourx = findElement(r'//*[@id="4108947"]')
+        #fourx.click()
         bar.send_keys('WEM')
         wem = findElement(r'//*[@id="4081860"]')
         wem.click()
-        bar.click()
-        bar.clear()
-        bar.send_keys('UT 1808')
-        ut = findElement(r'//*[@id="4059790"]')
-        ut.click()
+        wem2 = findElement(r'//*[@id="4220353"]')
+        wem2.click()
+        wem3 = findElement(r'//*[@id="4275915"]')
+        wem3.click()
+# =============================================================================
+#         bar.click()
+#         bar.clear()
+#         bar.send_keys('UT 1808')
+#         ut = findElement(r'//*[@id="4059790"]')
+#         ut.click()
+# =============================================================================
         ok = findElement(r'/html/body/div[137]/div[3]/div/button[2]/span')
         ok.click()
         viewreport = findElement(r'//*[@id="save-form"]')
         viewreport.click()
-        time.sleep(25)
-        cls = findElement(r'/html/body/div[contains(.,"Generating")]/div[3]/div/button')
-        cls.click()
+        time.sleep(15)
+        #cls = findElement(r'/html/body/div[contains(.,"Generating")]/div[3]/div/button')
+        #cls.click()
+        x = findElement(r'/html/body/div[1]/table/tbody/tr/td[2]/a')
+        x.click()
+        close = findElement(r'/html/body/div[147]/div[3]/div/button/span')
+        close.click()
 
         # Rename last download to ownership
         todayDate = datetime.date(datetime.now())
@@ -135,7 +148,7 @@ def updateLabels(webscrape, workingInterestFile, ownershipFile):
         #contreportbut.click()
         time.sleep(4)
         #workinginterest = findElement(r'//*[@id="ui-id-2"]/div[contains(.,"Working")]/a')
-        workinginterest = findElement(r'//*[@id="ui-id-1"]/div[39]/a')
+        workinginterest = findElement(r'//*[@id="ui-id-1"]/div[40]/a')
         workinginterest.click()
         target2 = findElement('//*[@id="filters"]/table/tbody/tr[1]/td[2]/div/a')
         target2 = str(target2.get_attribute('id'))
@@ -150,6 +163,8 @@ def updateLabels(webscrape, workingInterestFile, ownershipFile):
         wemii.click()
         wemi = findElement(r'//*[@id="90095"]')
         wemi.click()
+        wemiii = findElement(r'//*[@id="122549"]')
+        wemiii.click()
         okay = findElement(r'/html/body/div[contains(.,"OK")]/div[3]/div/button[2]')
         okay.click()
         target3 = findElement('//*[@id="filters"]/table/tbody/tr[3]/td[2]/div/a')
@@ -160,12 +175,15 @@ def updateLabels(webscrape, workingInterestFile, ownershipFile):
         search2 = findElement(f'//*[@id="data-picker-{ids3}-simple"]/div/input[2]')
         search2.click()
         search2.send_keys('WEM')
-        time.sleep(1)
-        #wem2 = findElement(r'//*[@id="115153"]')
-        wem2 = findElement(f'//*[@id="data-picker-{ids3}-multiple-results"]/ul/li[1]')
-        wem2.click()
-        wem1 = findElement(f'//*[@id="data-picker-{ids3}-multiple-results"]/ul/li[2]')
+        time.sleep(2)        
+        #wem1 = findElement(f'//*[@id="data-picker-{ids3}-multiple-results"]/ul/li[2]')
+        wem1 = findElement(r'//*[@id="90095"]')
         wem1.click()
+        wem2 = findElement(r'//*[@id="115153"]')
+        #wem2 = findElement(f'//*[@id="data-picker-{ids3}-multiple-results"]/ul/li[1]')
+        wem2.click()
+        wem3 = findElement(r'//*[@id="122549"]')
+        wem3.click()
         time.sleep(1)
         #okkk = findElement(r'/html/body/div[97]/div[3]/div/button[2]')
         okkk = findElement(r'/html/body/div[98]/div[3]/div/button[2]/span')
@@ -187,7 +205,7 @@ def updateLabels(webscrape, workingInterestFile, ownershipFile):
         #time.sleep(5)
         #downloadclick = findElement(r'//*[@id="ui-id-50"]/p/p/a')
         #downloadclick.click()
-        time.sleep(1000)
+        time.sleep(100)
 
         # Rename last download to leasehold
         todayDate = datetime.date(datetime.now())
@@ -198,42 +216,103 @@ def updateLabels(webscrape, workingInterestFile, ownershipFile):
         newleaseName = r'C:\\Users\\GISUser\\Downloads\\' + str(leasingDate)
         os.rename(leasehold, newleaseName)
 
+        
+# =============================================================================
+#This is the old directory that files were being saved to (for "Python Coding Labels v2" in QGIS).
+#         # MAKE A COPY OF THE PREVIOUS FILES AND STORE THEM IN A NEW FOLDER WITH THE DATE
+#         dir = r"//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/"
+#         todayDate = datetime.date(datetime.now())
+#         dateFolder = dir + str(todayDate) + ' Backup'
+#         # check if directory exists or not yet
+#         if not os.path.exists(dateFolder):
+#             os.makedirs(dateFolder)
+#         # move files into created directory
+#         file_pathLeasing = r"//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/" \
+#                            r"QGIS - WEM LEASE Crop List.csv"
+#         file_pathOwnership = r'//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/' \
+#                              r'QGIS - WEM, UT, 4X Import List.csv'
+#         shutil.copy(file_pathLeasing, dateFolder)
+#         shutil.copy(file_pathOwnership, dateFolder)
+#         # rename the files
+#         shutil.move(dateFolder + '/QGIS - WEM LEASE Crop List.csv',
+#                     dateFolder + '/QGIS - WEM LEASE Crop List (moved ' + str(todayDate) + ').csv')
+#         shutil.move(dateFolder + '/QGIS - WEM, UT, 4X Import List.csv',
+#                     dateFolder + '/QGIS - WEM, UT, 4X Import List (moved ' + str(todayDate) + ').csv')
+#         driver.quit()
+# 
+# =============================================================================
 
         # MAKE A COPY OF THE PREVIOUS FILES AND STORE THEM IN A NEW FOLDER WITH THE DATE
-        dir = r"//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/"
+        dir = r"//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/Python Coding Labels v3/"
         todayDate = datetime.date(datetime.now())
         dateFolder = dir + str(todayDate) + ' Backup'
         # check if directory exists or not yet
         if not os.path.exists(dateFolder):
             os.makedirs(dateFolder)
         # move files into created directory
-        file_pathLeasing = r"//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/" \
-                           r"QGIS - WEM LEASE Crop List.csv"
-        file_pathOwnership = r'//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/' \
-                             r'QGIS - WEM, UT, 4X Import List.csv'
-        shutil.copy(file_pathLeasing, dateFolder)
-        shutil.copy(file_pathOwnership, dateFolder)
+        file_LeasingWEM1 = r"//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/" \
+                           r"Python Coding Labels v3/WEM1Leasehold.csv"
+        file_pathOwnerWEM1 = r'//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/' \
+                             r'Python Coding Labels v3/WEM1Ownership.csv'
+        shutil.copy(file_LeasingWEM1, dateFolder)
+        shutil.copy(file_pathOwnerWEM1, dateFolder)
+        
+        file_LeasingWEM2 = r"//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/" \
+                           r"Python Coding Labels v3/WEM2Leasehold.csv"
+        file_pathOwnerWEM2 = r'//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/' \
+                             r'Python Coding Labels v3/WEM2Ownership.csv'
+        shutil.copy(file_LeasingWEM2, dateFolder)
+        shutil.copy(file_pathOwnerWEM2, dateFolder)
+        
+        file_LeasingWEM3 = r"//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/" \
+                           r"Python Coding Labels v3/WEM3Leasehold.csv"
+        file_pathOwnerWEM3 = r'//WEM-MASTER/Sensitive Data/WEM Uintah/Maps/QGIS/IMPORTANT EXCEL DOCS/QGIS/' \
+                             r'Python Coding Labels v3/WEM3Ownership.csv'
+        shutil.copy(file_LeasingWEM3, dateFolder)
+        shutil.copy(file_pathOwnerWEM3, dateFolder)
+        
         # rename the files
-        shutil.move(dateFolder + '/QGIS - WEM LEASE Crop List.csv',
-                    dateFolder + '/QGIS - WEM LEASE Crop List (moved ' + str(todayDate) + ').csv')
-        shutil.move(dateFolder + '/QGIS - WEM, UT, 4X Import List.csv',
-                    dateFolder + '/QGIS - WEM, UT, 4X Import List (moved ' + str(todayDate) + ').csv')
+        shutil.move(dateFolder + '/WEM1Leasehold.csv',
+                    dateFolder + '//WEM1Leasehold (moved ' + str(todayDate) + ').csv')
+        shutil.move(dateFolder + '/WEM1Ownership.csv',
+                    dateFolder + '//WEM1Ownership (moved ' + str(todayDate) + ').csv')
         driver.quit()
+
+        shutil.move(dateFolder + '/WEM2Leasehold.csv',
+                    dateFolder + '//WEM2Leasehold (moved ' + str(todayDate) + ').csv')
+        shutil.move(dateFolder + '/WEM2Ownership.csv',
+                    dateFolder + '//WEM2Ownership (moved ' + str(todayDate) + ').csv')
+        driver.quit()
+        
+        shutil.move(dateFolder + '/WEM3Leasehold.csv',
+                    dateFolder + '//WEM3Leasehold (moved ' + str(todayDate) + ').csv')
+        shutil.move(dateFolder + '/WEM3Ownership.csv',
+                    dateFolder + '//WEM3Ownership (moved ' + str(todayDate) + ').csv')
+        driver.quit()        
 
 
     if webscrape == False:
         newleaseName = workingInterestFile
         newownerName = ownershipFile
-    # RUN THE R SCRIPTS FIXING THE DATA FROM ILANDMAN TO QGIS
-    test = r"\\WEM-MASTER\Sensitive Data\WEM Uintah\Maps\Code\leasingLabels.R"
-    ro.r.source(test)
-    ro.r["leasingLabels"](newleaseName)  # Have the input to the function be the file path
-    fpOwnership = r"\\WEM-MASTER\Sensitive Data\WEM Uintah\Maps\Code\ownershipLabels.R"
-    ro.r.source(fpOwnership)
-    ro.r["ownershipLabels"](newownerName)
+        
+    labels(newownerName, r'X:\WEM Uintah\Maps\QGIS\IMPORTANT EXCEL DOCS\QGIS\Python Coding Labels v3')
+    leaseLabel(newleaseName, r'X:\WEM Uintah\Maps\QGIS\IMPORTANT EXCEL DOCS\QGIS\Python Coding Labels v3')
+    
+    
+        
+# =============================================================================
+# This is the old R code. this has been replaced by labels and leaseLabel         
+#     # RUN THE R SCRIPTS FIXING THE DATA FROM ILANDMAN TO QGIS
+#     test = r"\\WEM-MASTER\Sensitive Data\WEM Uintah\Maps\Code\leasingLabels.R"
+#     ro.r.source(test)
+#     ro.r["leasingLabels"](newleaseName)  # Have the input to the function be the file path
+#     fpOwnership = r"\\WEM-MASTER\Sensitive Data\WEM Uintah\Maps\Code\ownershipLabels.R"
+#     ro.r.source(fpOwnership)
+#     ro.r["ownershipLabels"](newownerName)
+# =============================================================================
 
 
     print("Done!")
 
 
-#updateLabels(True, '', '')
+updateLabels(True, '', '')
